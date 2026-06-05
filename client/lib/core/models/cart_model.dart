d5c8248121d6ce72ee6d29cfc137ea95503ea912
Pasan Kalhara <kalharap84@gmail.com>
2026-06-06 01:11:03 +0530

    feat: Implement CartItem constructor and fromJson factory method for cart functionality

--- client/lib/core/models/cart_model.dart
+++ client/lib/core/models/cart_model.dart
@@ -6,5 +6,23 @@ class CartItem {
   final String? productImageUrl;
   int quantity;
 
-  
+  CartItem({
+    required this.id,
+    required this.productId,
+    required this.productName,
+    required this.productPrice,
+    this.productImageUrl,
+    required this.quantity,
+  });
+
+  factory CartItem.fromJson(Map<String, dynamic> json) {
+    return CartItem(
+      id: json['id'],
+      productId: json['productId'],
+      productName: json['productName'],
+      productPrice: json['productPrice'].toDouble(),
+      productImageUrl: json['productImageUrl'],
+      quantity: json['quantity'],
+    );
+  }
 }
