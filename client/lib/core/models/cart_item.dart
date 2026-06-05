8c27e561968f8c4cbda2975fa926be915e8c9643
Pasan Kalhara <kalharap84@gmail.com>
2026-06-06 01:09:10 +0530

    feat: Define CartItem model with necessary fields for cart functionality

--- client/lib/core/models/cart_item.dart
+++ client/lib/core/models/cart_item.dart
@@ -1,3 +1,11 @@
 class CartItem {
+  final String id;
+  final String cartId;
+  final String productId;
+  final String productName;
+  final String productImage;
+  final double price;
+  final int quantity;
+  final DateTime createdAt;
   
 }
